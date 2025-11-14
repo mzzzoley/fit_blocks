861504742c89f337736df5d1cca392d497e3bd82
Zzzoley <mzzzoley@gmail.com>
2025-11-14 12:35:27 +0100

    add stats every solution

--- src/stats.py
+++ src/stats.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from src.app import FitBlocks
 from timer import timeit
 
@@ -17,6 +18,26 @@ def stats(print_boards: bool=False):
                 print(f'{m}.{d}', ' not found')
     print(success, ' found out of ', cnt)
 
+@timeit
+def stats_every_solution():
+    cnt = 0
+    row_list = []
+
+    for m in range(1, 13):
+        for d in range(1, 32):
+            cnt += 1
+            app = FitBlocks(m, d)
+            solution_count = app.solve(print_board=False, find_all=True)
+            row_list.append({'month':m,
+                             'day':d,
+                              'solution_count':solution_count})
+            print(f'{m}.{d}', f' {solution_count} solutions found')
+    result = pd.DataFrame(row_list, columns=('month', 'day', 'solution_count'))
+    result.sort_values('solution_count')
+    result.to_csv('every_result.csv')
+
+
 if __name__ == '__main__':
-    # stats(True)
-    stats()
+    stats(True)
+    # stats()
+    # stats_every_solution() # takes over 2 hours to create solution count for every day
